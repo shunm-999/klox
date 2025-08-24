ccfcf45a147735212826707d2969860926dca3b9
shunm-999 <shunmiyazaki7812@gmail.com>
2025-08-24 22:16:23 +0900

    add Scanner class for token scanning and Token data class for token representation

---
+++ src/main/kotlin/Scanner.kt
@@ -0,0 +1,53 @@
+class Scanner(private val source: String) {
+    private var start: Int = 0
+    private var current: Int = 0
+    private var line: Int = 1
+
+    private val tokens: MutableList<Token> = mutableListOf()
+
+    fun scanTokens(): List<Token> {
+        while (!isAtEnd()) {
+            start = current
+            scanToken()
+        }
+
+        tokens.add(Token(TokenType.EOF, "", null, line))
+        return tokens
+    }
+
+    private fun scanToken() {
+        val c = advance()
+
+        when (c) {
+            '(' -> addToken(TokenType.LEFT_PAREN)
+            ')' -> addToken(TokenType.RIGHT_PAREN)
+            '{' -> addToken(TokenType.LEFT_BRACE)
+            '}' -> addToken(TokenType.RIGHT_BRACE)
+            ',' -> addToken(TokenType.COMMA)
+            '.' -> addToken(TokenType.DOT)
+            '-' -> addToken(TokenType.MINUS)
+            '+' -> addToken(TokenType.PLUS)
+            ';' -> addToken(TokenType.SEMICOLON)
+            '*' -> addToken(TokenType.STAR)
+        }
+    }
+
+    private fun advance(): Char {
+        return source[current++]
+    }
+
+    private fun addToken(type: TokenType, literal: Any? = null) {
+        val text = source.substring(start, current)
+        val token = Token(
+            type = type,
+            lexeme = text,
+            literal = literal,
+            line = line,
+        )
+        tokens.add(token)
+    }
+
+    private fun isAtEnd(): Boolean {
+        return current >= source.length
+    }
+}
---
+++ src/main/kotlin/Token.kt
@@ -0,0 +1,6 @@
+data class Token(
+    val type: TokenType,
+    val lexeme: String,
+    val literal: Any?,
+    val line: Int,
+)
