320774b337b1bd2a54571f7129dc66db19d3539f
shunm-999 <shunmiyazaki7812@gmail.com>
2025-08-25 22:47:14 +0900

    replace main function implementation with expression evaluation and AST printing

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,13 @@
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    // TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
-
-    for (i in 1..5) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+    val expression = Expr.Binary(
+        left = Expr.Unary(
+            operator = Token(TokenType.MINUS, "-", null, 1),
+            right = Expr.Literal(123)
+        ),
+        operator = Token(TokenType.STAR, "*", null, 1),
+        right = Expr.Grouping(Expr.Literal(45.67))
+    )
+    println(AstPrinter().print(expression))
 }
