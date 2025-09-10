3b1b08d36fdde9eea3632a443b6fddbf7b40f19f
shunm-999 <shunmiyazaki7812@gmail.com>
2025-09-10 10:14:44 +0900

    implement binary expression evaluation in Interpretor class

--- src/main/kotlin/Interpretor.kt
+++ src/main/kotlin/Interpretor.kt
@@ -1,6 +1,59 @@
 class Interpretor : Expr.Visitor<Any?> {
     override fun visitBinaryExpr(expr: Expr.Binary): Any? {
-        TODO("Not yet implemented")
+        val left = evaluate(expr.left)
+        val right = evaluate(expr.right)
+
+        return when (expr.operator.type) {
+            TokenType.BANG_EQUAL -> {
+                !isEqual(left, right)
+            }
+
+            TokenType.EQUAL_EQUAL -> {
+                isEqual(left, right)
+            }
+
+            TokenType.GREATER -> {
+                (left as Double) > (right as Double)
+            }
+
+            TokenType.GREATER_EQUAL -> {
+                (left as Double) >= (right as Double)
+            }
+
+            TokenType.LESS -> {
+                (left as Double) < (right as Double)
+            }
+
+            TokenType.LESS_EQUAL -> {
+                (left as Double) <= (right as Double)
+            }
+
+            TokenType.MINUS -> {
+                (left as Double) - (right as Double)
+            }
+
+            TokenType.PLUS -> {
+                if (left is Double && right is Double) {
+                    left + right
+                }
+                if (left is String && right is String) {
+                    left + right
+                }
+                throw IllegalStateException("Unexpected token: $left + $right")
+            }
+
+            TokenType.SLASH -> {
+                (left as Double) / (right as Double)
+            }
+
+            TokenType.STAR -> {
+                (left as Double) * (right as Double)
+            }
+
+            else -> {
+                throw IllegalStateException("Unexpected operator ${expr.operator}")
+            }
+        }
     }
 
     override fun visitGroupingExpr(expr: Expr.Grouping): Any? {
@@ -41,5 +94,15 @@ class Interpretor : Expr.Visitor<Any?> {
             return target
         }
         return true
+    }
+
+    private fun isEqual(a: Any?, b: Any?): Boolean {
+        if (a == null && b == null) {
+            return true
+        }
+        if (a == null) {
+            return false
+        }
+        return a == b
     }
 }
