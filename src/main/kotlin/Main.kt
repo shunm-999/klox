62192c085910ee7a77f9f10c40d090526b401867
shunm-999 <shunmiyazaki7812@gmail.com>
2025-09-10 11:21:58 +0900

    add interpretation of binary and unary expressions in main function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,12 @@
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
     val expression = Expr.Binary(
         left = Expr.Unary(
             operator = Token(TokenType.MINUS, "-", null, 1),
-            right = Expr.Literal(123)
+            right = Expr.Literal(123.0)
         ),
         operator = Token(TokenType.STAR, "*", null, 1),
         right = Expr.Grouping(Expr.Literal(45.67))
     )
     println(AstPrinter().print(expression))
+    println(Interpretor().interpret(expression))
 }
